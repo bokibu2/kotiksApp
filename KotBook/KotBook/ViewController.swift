6841c51e3fb3dd8c115fc2bf4808c9d116f7fbc4
Aliaksandr Niakhai <sashanehai@gmail.com>
2019-12-21 10:23:24 +0300

    Added sample project

---
+++ KotBook/KotBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KotBook
+//
+//  Created by Александр Нехай on 12/21/19.
+//  Copyright © 2019 kotiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
